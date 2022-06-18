4600befb5f80fb132364f9ac48af2955b37fee2d
Pavol Senko <pavol.senko@gmail.com>
2022-06-18 22:15:15 +0200

    Update Proxy.php

--- Proxy.php
+++ Proxy.php
@@ -411,6 +411,23 @@ class Proxy
             }
         }
 
+		if (isset($_SERVER['HTTP_ORIGIN'])) {
+			header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+			header('Access-Control-Allow-Credentials: true');
+			header('Access-Control-Max-Age: 86400');
+		}
+
+		if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+
+			if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD'])) {
+				header("Access-Control-Allow-Methods: GET, OPTIONS");   
+			}				
+
+			if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS'])) {
+				header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+			}
+		}
+
         http_response_code($responseCode);
 
         //----------------------------------
@@ -457,14 +474,13 @@ class Proxy
     }
 }
 
-if (!Proxy::isInstalledWithComposer()) {
-    Proxy::checkCompatibility();
-    Proxy::registerErrorHandlers();
-    $responseCode = Proxy::run();
 
-    if (Proxy::isResponseCodeOk($responseCode)) {
-        exit(0);
-    } else {
-        exit($responseCode);
-    }
+Proxy::checkCompatibility();
+Proxy::registerErrorHandlers();
+$responseCode = Proxy::run();
+
+if (Proxy::isResponseCodeOk($responseCode)) {
+    exit(0);
+} else {
+    exit($responseCode);
 }
